92ad0a00ae328b3aee53a6cd759e14da877bcf59
arefinMilen <arefin15-5279@diu.edu.bd>
2026-06-10 16:11:53 +0600

    using static members for no uses of instance of the class

---
+++ classAndObject/staticMembers.js
@@ -0,0 +1,11 @@
+var arefin = /** @class */ (function () {
+    function arefin() {
+    }
+    arefin.getCompany = function () {
+        return Math.random();
+    };
+    arefin.company = "Arefin IT";
+    return arefin;
+}());
+//using static members no need to create an instance of the class like 
+//let a1 = new arefin();
---
+++ classAndObject/staticMembers.ts
@@ -0,0 +1,9 @@
+class arefin {
+  static company: string = "Arefin IT";
+
+  static getCompany() {
+    return Math.random();
+  }
+}
+//using static members no need to create an instance of the class like 
+//let a1 = new arefin();
